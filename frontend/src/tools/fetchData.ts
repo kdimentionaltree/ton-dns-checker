da47793c457146314dc76fca8a495315bc7927e4
timxag <timxag@yandex.ru>
2022-11-12 23:46:34 +0300

    change api url

--- frontend/src/tools/fetchData.ts
+++ frontend/src/tools/fetchData.ts
@@ -1,4 +1,4 @@
-export const API_URL = "https://toncenter.kdimentionaltree.com";
+export const API_URL = "";
 export const fetchDHTData = async () => {
   const response = await fetch(`${API_URL}/api/dns/dhts`);
   const data = await response.json();
