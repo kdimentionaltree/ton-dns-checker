07df1c332b2add69d97b8f208b3260df8019bf34
Doge <awesomedogewow@gmail.com>
2024-01-01 01:35:23 +0800

    feat: Add script to download TON configuration files
    This commit adds a script that downloads the TON mainnet and testnet configuration files. The script creates a directory to store the configuration files if it doesn't exist, and then uses `wget` to download the files. The downloaded files are saved as `private/mainnet.json` and `private/testnet.json`.

--- init.sh
+++ init.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
+
+# 退出脚本，如果任何命令执行失败
 set -e
 
+# 创建一个目录来存储配置文件，如果目录不存在
 mkdir -p private
 
-wget https://ton.org/global-config.json -q -O private/mainnet.json  # mainnet config
-wget https://ton-blockchain.github.io/testnet-global.config.json -q -O private/testnet.json # testnet config
+# 下载 TON 主网配置文件
+# -q: 安静模式，不输出任何信息
+# -O: 指定输出文件名
+echo "Downloading TON mainnet configuration..."
+wget https://ton.org/global-config.json -q -O private/mainnet.json
+
+# 下载 TON 测试网配置文件
+echo "Downloading TON testnet configuration..."
+wget https://ton-blockchain.github.io/testnet-global.config.json -q -O private/testnet.json
