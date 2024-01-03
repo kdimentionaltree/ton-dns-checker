a2ed0c12e6f7b6bfd3ccddb68c79715a019cefa7
Doge <awesomedogewow@gmail.com>
2024-01-03 15:08:08 +0800

    feat: Add CORS middleware and create FastAPI instance
    This commit adds the CORS middleware to allow all origins, methods, and headers. It also creates a FastAPI instance.

--- dnschecker/main.py
+++ dnschecker/main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI, Request, Depends
+from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 
 import time
 import logging
@@ -8,11 +9,22 @@ import asyncio
 from dnschecker.api.deps import dns_resolver_dep
 from dnschecker.api.dns import api as dns_api
 
+# 创建 FastAPI 实例
+app = FastAPI()
+
+# 添加 CORS 中间件
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 允许所有源
+    allow_credentials=True,
+    allow_methods=["*"],  # 允许所有方法
+    allow_headers=["*"],  # 允许所有头部
+)
 
 logger = logging.getLogger(__name__)
 
+# 其他路由和事件处理器
 
-app = FastAPI()
 @app.middleware("http")
 async def add_process_time_header(request: Request, call_next):
     if not request.base_url == '/healthcheck':
@@ -33,11 +45,9 @@ async def startup_event():
     await dns_resolver_dep.init(loop)
     logger.info('DNS Resolver inited')
 
-
 @app.get('/healthcheck')
 def healthcheck():
     return 'OK'
-
 
 app.mount('/api/dns', dns_api)
 app.mount('/dns', StaticFiles(directory="/app/static", html=True), name="frontend")
